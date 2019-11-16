fac332538467de2f0f45959844fb97c80365e508
Ante <tolusic9@gmail.com>
2019-11-16 15:37:14 +0100

    Add transaction for user creation

--- repo/user.js
+++ repo/user.js
@@ -1,18 +1,20 @@
 const bcrypt = require('bcryptjs')
 const _ = require('lodash')
+const { transaction } = require('objection')
 
 const error = require('error')
 const { trimSpacesGlobally } = require('util/string')
 const { findOneResolver } = require('db/util')
 
 const User = require('db/model/User')
+const knex = User.knex()
 
 async function hashPassword (password) {
   return bcrypt.hash(password, _.toInteger(process.env.BCRYPT_ROUNDS))
 }
 
-async function create ({ password, fullname, email }) {
-  const user = await User.query().where('fullname', fullname).orderBy('createdAt')
+async function generateUsernameFromFullname (fullname, trx = knex) {
+  const user = await User.query(trx).where('fullname', fullname).orderBy('createdAt')
   const len = user.length
 
   let generatedUsername
@@ -28,19 +30,28 @@ async function create ({ password, fullname, email }) {
     generatedUsername = trimSpacesGlobally(fullname)
   }
 
-  return User.query().insert({
-    email,
-    fullname,
-    username: generatedUsername,
-    password: await hashPassword(password),
-  })
-  .catch(err => {
-    switch (err.constraint) {
-      case 'user_email_unique':
-        throw error('user.duplicate', err)
-      default:
-        throw error.db(err)
-    }
+  return generatedUsername
+}
+
+async function create ({ password, fullname, email }) {
+  return transaction(knex, async trx => {
+    const username = await generateUsernameFromFullname(fullname, trx)
+    const user = await User.query(trx).insert({
+      email,
+      fullname,
+      username,
+      password: await hashPassword(password),
+    })
+    .catch(err => {
+      switch (err.constraint) {
+        case 'user_email_unique':
+          throw error('user.duplicate', err)
+        default:
+          throw error.db(err)
+      }
+    })
+
+    return user
   })
 }
 
